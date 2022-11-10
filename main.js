d0e5398c47bba5dc5f001db2f33c9e6c60c91fa0
MarcoPerta <perta.marco@gmail.com>
2022-11-10 16:14:31 +0100

    done

---
+++ main.js
@@ -0,0 +1,17 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+var app = new Vue(
+    {
+      el: '#root',
+      data: {
+       message: 'Ciao Mondo!',
+       image: 'artem-labunsky-miW5lfeiErE-unsplash.jpg'
+     },
+      method: {
+
+     }
+    }
+ );
